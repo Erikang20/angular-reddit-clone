cec5ce98367b26310689dfe5163c9cb0b73867a4
Erikang20 <erikang20@gmail.com>
2016-07-15 16:47:21 -0500

    add puppy

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 var app = angular.module( "clone", [] );
 
 app.controller( "redditClone", function( $scope ) {
-	$scope.post = {};
+	$scope.clone = {};
+	$scope.posts = [];
+	$scope.rightNow = Date.now();
+	$scope.propertyName = 'title';
+	$scope.reverse = true;
+
+
+	$scope.addPost = function() {
+		var newPost = {};
+		newPost.title = $scope.clone.title;
+		newPost.author = $scope.clone.author;
+		newPost.img = $scope.clone.img;
+		newPost.description = $scope.clone.description;
+
+		$scope.posts.push( newPost );
+		console.log( $scope.posts );
+		console.log( "working here yo!" );
+	}
+
+	$scope.sortBy = function() {
+		$scope.reverse = ( $scope.propertyName === propertyName ) ? !$scope.reverse : false;
+		$scope.propertyName = propertyName;
+	}
+
 } );
