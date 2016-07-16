bd2b8e876954c4a8a158be0d587881b978724d2d
Erikang20 <erikang20@gmail.com>
2016-07-15 19:41:32 -0500

    add comments

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ var app = angular.module( "clone", [] );
 app.controller( "redditClone", function( $scope ) {
 	$scope.clone = {};
 	$scope.posts = [];
+	// $scope.comment = [];
+	$scope.comments = [];
 	$scope.rightNow = Date.now();
 	$scope.propertyName = 'title';
 	$scope.reverse = true;
 	$scope.view = {};
 	$scope.view.enterCount = 0;
 	$scope.view.enterCount2 = 0;
-
+	$scope.view.enterCount3 = 0;
 
 
 	$scope.generate = function() {
@@ -28,6 +30,16 @@ app.controller( "redditClone", function( $scope ) {
 		$scope.posts.push( newPost );
 		console.log( $scope.posts );
 		console.log( "working here yo!" );
+	}
+
+	$scope.addComment = function() {
+		var newComment = {};
+		newComment.comment = $scope.clone.comment;
+
+		$scope.comments.push( newComment );
+
+		console.log( $scope.clone.comment );
+		console.log( "working there too" );
 	}
 
 	$scope.sortBy = function() {
