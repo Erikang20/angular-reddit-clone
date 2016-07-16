00e6315dfaba331e69ccf6150a44bdd9b7922a7a
Erikang20 <erikang20@gmail.com>
2016-07-16 17:29:19 -0500

    add changes on date

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.controller( "redditClone", function( $scope ) {
 	$scope.sortReverse = false;
 	$scope.searchPost = '';
 	$scope.comments = [];
-	$scope.rightNow = Date.now();
+	$scope.date = Date.now();
 	$scope.propertyName = 'title';
 	$scope.reverse = true;
 	$scope.view = {};
@@ -15,25 +15,27 @@ app.controller( "redditClone", function( $scope ) {
 	$scope.view.enterCount2 = 0;
 	$scope.view.enterCount3 = 0;
 
-
-	var post = [
-		{
-			title: 'puppies',
-			author: 'Taco',
-			img: "https://img.buzzfeed.com/buzzfeed-static/static/2015-02/19/10/campaign_images/webdr07/17-puppies-who-are-so-cute-they-will-make-you-mad-2-30501-1424359138-6_dblbig.jpg",
-			description: 'sunt in culpa qui officia deserunt mollit anim id est laborum jajhajdshhyuhfnadfuasdbuashdfuihsdkjnjsdbhs ',
-			date: 07 / 16 / 2016,
-			score: 9
+	function dummy() {
+		var dog = dogs;
+		dogs = [
+			{
+				title: 'puppies',
+				author: 'Taco',
+				img: "https://img.buzzfeed.com/buzzfeed-static/static/2015-02/19/10/campaign_images/webdr07/17-puppies-who-are-so-cute-they-will-make-you-mad-2-30501-1424359138-6_dblbig.jpg",
+				description: 'sunt in culpa qui officia deserunt mollit anim id est laborum jajhajdshhyuhfnadfuasdbuashdfuihsdkjnjsdbhs ',
+				date: 07 / 16 / 2016,
+				score: 9
 	},
-		{
-			title: 'more puppies',
-			author: 'Jack',
-			img: 'http://www.puppiesinflorida.com/wp-content/uploads/2016/07/Puppies_for_sale_in_florida.jpg',
-			description: 'sunt in culpa qui officia deserunt mollit anim id est laborum jajhajdshhyuhfnadfuasdbuashdfuihsdkjnjsdbhs ',
-			date: 04 / 14 / 206,
-			score: 10
+			{
+				title: 'more puppies',
+				author: 'Jack',
+				img: 'http://www.puppiesinflorida.com/wp-content/uploads/2016/07/Puppies_for_sale_in_florida.jpg',
+				description: 'sunt in culpa qui officia deserunt mollit anim id est laborum jajhajdshhyuhfnadfuasdbuashdfuihsdkjnjsdbhs ',
+				date: 04 / 14 / 206,
+				score: 10
 		}
    ];
+	}
 
 
 	$scope.generate = function() {
@@ -43,18 +45,24 @@ app.controller( "redditClone", function( $scope ) {
 
 
 	$scope.addPost = function() {
-		var newPost = {};
-		newPost.title = $scope.clone.title;
-		newPost.author = $scope.clone.author;
-		newPost.img = $scope.clone.img;
-		newPost.description = $scope.clone.description;
+			var newPost = {};
+			newPost.title = $scope.clone.title;
+			newPost.author = $scope.clone.author;
+			newPost.img = $scope.clone.img;
+			newPost.description = $scope.clone.description;
 
-		$scope.posts.push( newPost );
-		console.log( $scope.posts );
-		console.log( "working here yo!" );
+			$scope.posts.push( newPost );
+			console.log( $scope.posts );
+			console.log( "working here yo!" );
 
-	}
-
+		}
+		// $scope.signupForm = function() {
+		// 	if ( $scope.userForm.$valid ) {
+		// 		// Submit as normal
+		// 	} else {
+		// 		$scope.userForm.submitted = true;
+		// 	}
+		// }
 
 
 
