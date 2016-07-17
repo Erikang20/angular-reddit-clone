d8afa547d7eb9f53ef0698c31a07cfd9189284fe
Erikang20 <erikang20@gmail.com>
2016-07-17 12:20:33 -0500

    clear form

--- app.js
+++ app.js
@@ -8,34 +8,40 @@ app.controller( "redditClone", function( $scope ) {
 	$scope.searchPost = '';
 	$scope.comments = [];
 	$scope.date = Date.now();
-	$scope.propertyName = 'title';
 	$scope.reverse = true;
 	$scope.view = {};
 	$scope.view.enterCount = 0;
 	$scope.view.enterCount2 = 0;
 	$scope.view.enterCount3 = 0;
 
-	function dummy() {
-		var dog = dogs;
-		dogs = [
-			{
-				title: 'puppies',
-				author: 'Taco',
-				img: "https://img.buzzfeed.com/buzzfeed-static/static/2015-02/19/10/campaign_images/webdr07/17-puppies-who-are-so-cute-they-will-make-you-mad-2-30501-1424359138-6_dblbig.jpg",
-				description: 'sunt in culpa qui officia deserunt mollit anim id est laborum jajhajdshhyuhfnadfuasdbuashdfuihsdkjnjsdbhs ',
-				date: 07 / 16 / 2016,
-				score: 9
-	},
-			{
-				title: 'more puppies',
-				author: 'Jack',
-				img: 'http://www.puppiesinflorida.com/wp-content/uploads/2016/07/Puppies_for_sale_in_florida.jpg',
-				description: 'sunt in culpa qui officia deserunt mollit anim id est laborum jajhajdshhyuhfnadfuasdbuashdfuihsdkjnjsdbhs ',
-				date: 04 / 14 / 206,
-				score: 10
-		}
-   ];
-	}
+
+
+	// function dummy() {
+	// 	var isThis = this;
+	// array = [
+	// 	{
+	// 		title: 'puppies',
+	// 		author: 'Taco',
+	// 		img: "https://img.buzzfeed.com/buzzfeed-static/static/2015-02/19/10/campaign_images/webdr07/17-puppies-who-are-so-cute-they-will-make-you-mad-2-30501-1424359138-6_dblbig.jpg",
+	// 		description: 'sunt in culpa qui officia deserunt mollit anim id est laborum jajhajdshhyuhfnadfuasdbuashdfuihsdkjnjsdbhs ',
+	// 		date: 07 / 16 / 2016,
+	// 		score: 9
+	// },
+	// 	{
+	// 		title: 'more puppies',
+	// 		author: 'Jack',
+	// 		img: 'http://www.puppiesinflorida.com/wp-content/uploads/2016/07/Puppies_for_sale_in_florida.jpg',
+	// 		description: 'sunt in culpa qui officia deserunt mollit anim id est laborum jajhajdshhyuhfnadfuasdbuashdfuihsdkjnjsdbhs ',
+	// 		date: 04 / 14 / 206,
+	// 		score: 10
+	// 	}
+	//  ];
+
+
+
+
+
+	// }
 
 
 	$scope.generate = function() {
@@ -44,37 +50,54 @@ app.controller( "redditClone", function( $scope ) {
 	};
 
 
+	var reset = function() {
+		// $scope.clone = angular.copy( $scope.originForm );
+		// $scope.clone.$setPristine();
+		$scope.clone.title = null;
+		$scope.clone.author = null;
+		$scope.clone.img = null;
+		$scope.clone.description = null;
+		$scope.clone.userForm = false;
+
+		console.log( "this is clean!" );
+	}
+
+
+
 	$scope.addPost = function() {
-			var newPost = {};
-			newPost.title = $scope.clone.title;
-			newPost.author = $scope.clone.author;
-			newPost.img = $scope.clone.img;
-			newPost.description = $scope.clone.description;
+		var newPost = {};
+		newPost.title = $scope.clone.title;
+		newPost.author = $scope.clone.author;
+		newPost.img = $scope.clone.img;
+		newPost.description = $scope.clone.description;
 
-			$scope.posts.push( newPost );
-			console.log( $scope.posts );
-			console.log( "working here yo!" );
+		$scope.posts.push( newPost );
+		console.log( $scope.posts );
+		reset();
+		console.log( "working here yo!" );
 
-		}
-		// $scope.signupForm = function() {
-		// 	if ( $scope.userForm.$valid ) {
-		// 		// Submit as normal
-		// 	} else {
-		// 		$scope.userForm.submitted = true;
-		// 	}
-		// }
-
-
+	}
 
 	$scope.addComment = function() {
 		var newComment = {};
 		newComment.comment = $scope.clone.comment;
-
 		$scope.comments.push( newComment );
 
 		console.log( $scope.clone.comment );
 		console.log( "working there too" );
 	}
+
+	// $scope.clone = {
+	// 	'title': '',
+	// 	'author': '',
+	// 	'img': '',
+	// 	'description': '',
+	// 	'comments': ''
+	// }
+
+	// $scope.originForm = angular.copy( $scope.clone );
+
+
 
 	$scope.sortBy = function() {
 		$scope.reverse = ( $scope.propertyName === propertyName ) ? !$scope.reverse : false;
