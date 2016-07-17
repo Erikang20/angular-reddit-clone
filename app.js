ef6acc77394c37c94e679aa6329e7232f032c2ee
Erikang20 <erikang20@gmail.com>
2016-07-17 17:28:23 -0500

    orderBy

--- app.js
+++ app.js
@@ -28,16 +28,33 @@ app.controller( "redditClone", function( $scope ) {
 			img: "https://img.buzzfeed.com/buzzfeed-static/static/2015-02/19/10/campaign_images/webdr07/17-puppies-who-are-so-cute-they-will-make-you-mad-2-30501-1424359138-6_dblbig.jpg",
 			description: 'sunt in culpa qui officia deserunt mollit anim id est laborum jajhajdshhyuhfnadfuasdbuashdfuihsdkjnjsdbhs ',
 			date: 07 / 16 / 2016,
-			score: 9
+			vote: 9
 	},
 		{
 			title: 'More puppies',
 			author: 'Jack',
 			img: 'http://www.puppiesinflorida.com/wp-content/uploads/2016/07/Puppies_for_sale_in_florida.jpg',
-			description: 'sunt in culpa qui officia deserunt mollit anim id est laborum jajhajdshhyuhfnadfuasdbuashdfuihsdkjnjsdbhs ',
+			description: 'sunt in culpa  sfbjhasfdhsdfjsak sfdsuhfisjfsmdfk sdfkshdfjmsdf s sifjismfkasndfjn kfiasjfdoiasdkfads qui officia deserunt mollit anim id est laborum jajhajdshhyuhfnadfuasdbuashdfuihsdkjnjsdbhs ',
 			date: 04 / 14 / 2015,
-			score: 10
-		}
+			vote: 3
+		},
+		{
+			title: 'Friends',
+			author: 'Chocolate',
+			img: 'https://s-media-cache-ak0.pinimg.com/236x/5e/82/e6/5e82e68b741b5a6384bb799cc8e03fe9.jpg',
+			description: 'hsbdhfgayfhausf ssfbsadfhausjfkas sfdbushfisadfams sfdbasfoaisf sunt in culpa qui officia deserunt mollit anim id est laborum ',
+			date: 05 / 14 / 2016,
+			vote: 10
+    },
+
+		{
+			title: 'Underwater',
+			author: 'Magnolia',
+			img: 'http://a.fastcompany.net/multisite_files/fastcompany/imagecache/slideshow_large/slideshow/2014/09/3036127-slide-s-2-feel-the-puppy-love-with-these-underwatercoreypromo.jpg',
+			description: 'more hsjhsdfasdfij asdfmasdkfnaskj dfnasdfnisad nkjasdnfjansdjfnsjfnasjdf sunt in culpa qui officia deserunt mollit anim id est laborum ',
+			date: 03 / 29 / 2014,
+			vote: 8
+    }
 	 ];
 
 
