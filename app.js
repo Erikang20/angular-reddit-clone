57e97c7a5b4dc9d98b06866d9cdecad315eb9a33
Erikang20 <erikang20@gmail.com>
2016-07-17 12:23:42 -0500

    add cute puppies

--- app.js
+++ app.js
@@ -18,30 +18,26 @@ app.controller( "redditClone", function( $scope ) {
 
 	// function dummy() {
 	// 	var isThis = this;
-	// array = [
-	// 	{
-	// 		title: 'puppies',
-	// 		author: 'Taco',
-	// 		img: "https://img.buzzfeed.com/buzzfeed-static/static/2015-02/19/10/campaign_images/webdr07/17-puppies-who-are-so-cute-they-will-make-you-mad-2-30501-1424359138-6_dblbig.jpg",
-	// 		description: 'sunt in culpa qui officia deserunt mollit anim id est laborum jajhajdshhyuhfnadfuasdbuashdfuihsdkjnjsdbhs ',
-	// 		date: 07 / 16 / 2016,
-	// 		score: 9
-	// },
-	// 	{
-	// 		title: 'more puppies',
-	// 		author: 'Jack',
-	// 		img: 'http://www.puppiesinflorida.com/wp-content/uploads/2016/07/Puppies_for_sale_in_florida.jpg',
-	// 		description: 'sunt in culpa qui officia deserunt mollit anim id est laborum jajhajdshhyuhfnadfuasdbuashdfuihsdkjnjsdbhs ',
-	// 		date: 04 / 14 / 206,
-	// 		score: 10
-	// 	}
-	//  ];
+	$scope.clone.post = [
+		{
+			title: 'puppies',
+			author: 'Taco',
+			img: "https://img.buzzfeed.com/buzzfeed-static/static/2015-02/19/10/campaign_images/webdr07/17-puppies-who-are-so-cute-they-will-make-you-mad-2-30501-1424359138-6_dblbig.jpg",
+			description: 'sunt in culpa qui officia deserunt mollit anim id est laborum jajhajdshhyuhfnadfuasdbuashdfuihsdkjnjsdbhs ',
+			date: 07 / 16 / 2016,
+			score: 9
+	},
+		{
+			title: 'more puppies',
+			author: 'Jack',
+			img: 'http://www.puppiesinflorida.com/wp-content/uploads/2016/07/Puppies_for_sale_in_florida.jpg',
+			description: 'sunt in culpa qui officia deserunt mollit anim id est laborum jajhajdshhyuhfnadfuasdbuashdfuihsdkjnjsdbhs ',
+			date: 04 / 14 / 206,
+			score: 10
+		}
+	 ];
 
 
-
-
-
-	// }
 
 
 	$scope.generate = function() {
@@ -51,8 +47,6 @@ app.controller( "redditClone", function( $scope ) {
 
 
 	var reset = function() {
-		// $scope.clone = angular.copy( $scope.originForm );
-		// $scope.clone.$setPristine();
 		$scope.clone.title = null;
 		$scope.clone.author = null;
 		$scope.clone.img = null;
